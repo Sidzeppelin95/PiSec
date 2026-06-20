abd679c18874a7bb36111c8c2a4d17883d5c316b
Sidzeppelin95 <50958322+Sidzeppelin95@users.noreply.github.com>
2026-06-21 00:26:34 +0530

    Update app.js

--- pishield/frontend/app.js
+++ pishield/frontend/app.js
@@ -1,1 +1,30 @@
+const Pi = window.Pi;
 
+Pi.init({
+    version: "2.0",
+    sandbox: true
+});
+
+async function authenticateUser() {
+
+    try {
+
+        const scopes = ['username', 'payments'];
+
+        const auth = await Pi.authenticate(
+            scopes,
+            onIncompletePaymentFound
+        );
+
+        console.log(auth);
+
+    } catch (err) {
+
+        console.error(err);
+    }
+}
+
+function onIncompletePaymentFound(payment) {
+
+    console.log(payment);
+}
